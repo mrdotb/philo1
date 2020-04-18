d449361614b7819ddb43275c5073bdfb6f73c1e2
mrdotb <mrdotb@protonmail.com>
2020-04-19 01:02:49 +0700

    init

--- philo_one/main.c
+++ philo_one/main.c
@@ -12,118 +12,76 @@
 
 #include "philo_one.h"
 
-
-static void print(t_philo *philo, char* action)
+int	usage(void)
 {
-	static struct timeval	start;
-	struct timeval 			tv;
-
-	if (gettimeofday(&tv, NULL) == 0)
-		printf("%ld.%06ld %d %s\n", tv.tv_sec, tv.tv_usec, philo->thread_nb, action);
+	ft_putstr_fd("philo_one\n", 2);
+	ft_putstr_fd("Usage:\n", 2);
+	ft_putstr_fd("philo_one number_of_philosopher time_to_die time_to_eat", 2);
+	ft_putstr_fd(" time_to_sleep", 2);
+	ft_putstr_fd(" [number_of_time_each_philosophers_must_eat]\n", 2);
+	return (1);
 }
 
-static void	*thread_start(void *arg)
+int	parse_arg(t_args *args, int argc, char **argv)
 {
-	t_philo	*philo;
-	/* struct timeval tv; */
-	philo = (t_philo*)arg;
-	while (philo->meals < MUST_EAT)
+	static int	i = 1;
+	static int	args_tab[5] = {0, 0, 0, 0, 0};
+
+	if (argc < 5 || argc > 6)
+		return (usage());
+	while (i < argc)
 	{
-		pthread_mutex_lock(&philo->left_fork);
-		print(philo, "has taken left fork");
-		pthread_mutex_lock(&philo->right_fork);
-		print(philo, "has taken right fork");;
-		print(philo, "is eating");
-		usleep(TIME_TO_EAT * MSEC);
-		pthread_mutex_unlock(&philo->left_fork);
-		pthread_mutex_unlock(&philo->right_fork);
-		/* if (gettimeofday(&tv, NULL) != 0) */
-			/* return NULL; */
-		/* printf("last meal: %ldsec %dmilli %dsec now - last meal = %ld\n", philo.last_meal.tv_sec, TIME_TO_DIE, TIME_TO_DIE / 1000, tv.tv_sec - philo.last_meal.tv_sec); */
-		if (gettimeofday(&(philo->last_meal), NULL) != 0)
-			return NULL;
-		/* printf("%d last meal %ld\n", philo->thread_nb, philo->last_meal.tv_sec); */
-		/* philo->last_meal.tv_sec = tv.tv_sec; */
-		/* philo->last_meal.tv_usec = tv.tv_usec; */
-		philo->meals++;
-		print(philo, "is sleeping");
-		usleep(TIME_TO_SLEEP * MSEC);
-		print(philo, "is thinking");
-	}
-	return NULL;
-}
-
-void	*death_check(t_philo *philos)
-{
-
-	printf("death_check philos is at address: %p\n", (void*)&philos);
-	int i;
-	struct timeval tv;
-
-	while (1)
-	{
-		if (gettimeofday(&tv, NULL) != 0)
-			return (NULL);
-		usleep(10 * MSEC);
-		i = 0;
-		while (i < NB_PHILO)
-		{
-			if (tv.tv_sec - (philos)[i].last_meal.tv_sec > TIME_TO_DIE / 1000)
-			{
-				printf("last meal: %ldsec now: %ldsec %dsec now - last meal = %ld\n", (philos)[i].last_meal.tv_sec, tv.tv_sec, TIME_TO_DIE / 1000, tv.tv_sec - (philos[i]).last_meal.tv_sec);
-				print(&(philos)[i], "is dead");
-				exit(1);
-			}
-			i++;
-		}
-	}
-	return NULL;
-}
-
-int	main(void)
-{
-	t_philo		philos[NB_PHILO];
-	int					i;
-
-	printf("main philos is at address: %p\n", (void*)&philos);
-	i = 0;
-	while (i < NB_PHILO)
-	{
-		philos[i].thread_nb = i + 1;
-		/* philos[i].state = THINK; */
-		philos[i].meals = 0;
-		if (gettimeofday(&philos[i].last_meal, NULL) != 0)
-			return (1);
-		if (i == 0) {
-			if (pthread_mutex_init(&philos[i].left_fork, NULL) != 0)
-				return(1);
-			if (pthread_mutex_init(&philos[i].right_fork, NULL) != 0)
-				return(1);
-		} else if (i == NB_PHILO){
-			philos[i].left_fork = philos[i - 1].right_fork;
-			philos[i].right_fork = philos[0].left_fork;
-		} else {
-			philos[i].left_fork = philos[i - 1].right_fork;
-			if (pthread_mutex_init(&philos[i].right_fork, NULL) != 0)
-				return(1);
-		}
-
-		pthread_create(&philos[i].thread_id, NULL, &thread_start, &(philos[i]));
-		printf("main philo[%d] is at address: %p\n", i, (void*)&philos[i]);
+		args_tab[i - 1] = ft_atoi(argv[i]);
+		if (args_tab[i - 1] < 1)
+			return (usage());
 		i++;
 	}
+	args->number_of_philosopher = args_tab[0];
+	args->time_to_die = args_tab[1];
+	args->time_to_eat = args_tab[2];
+	args->time_to_sleep = args_tab[3];
+	args->must_eat = args_tab[4];
+	return (0);
+}
 
-	death_check(philos);
+int	main(int argc, char **argv)
+{
+	t_args			args;
+	t_philo			**philos;
+	pthread_t		death_thread_id;
+	unsigned int	i;
+	int				*death_result;
+
+	if (parse_arg(&args, argc, argv) == 1)
+		return (1);
+	if ((philos = create_philos(&args)) == NULL)
+		return (1);
 	i = 0;
-	while (i < NB_PHILO)
+	while (i < args.number_of_philosopher)
 	{
-		pthread_join(philos[i].thread_id, NULL);
+		pthread_create(&philos[i]->thread_id, NULL, &thread_start, philos[i]);
 		i++;
 	}
-
+	pthread_create(&death_thread_id, NULL, &death_thread, philos);
+	pthread_join(death_thread_id, (void**)&death_result);
+	printf("pthread death_join death_result%d\n", *death_result);
+	if (death_result == NULL)
+		return 1;
+	if (*death_result == 1)
+	{
+		free_philos(philos, &args);
+		free(death_result);
+		return 0;
+	}
+	free(death_result);
 	i = 0;
-	/* while (i < NB_PHILO) */
-	/* 	printf("%d last meal %ld\n", philos[i].thread_nb, philos[i].last_meal.tv_sec); */
-	/* pthread_mutex_destroy(&lock); */
+	while (i < args.number_of_philosopher)
+	{
+		pthread_join(philos[i]->thread_id, NULL);
+		printf("pthread_join %d\n", i);
+		i++;
+	}
+	if (free_philos(philos, &args) == 1)
+		return (1);
 	return (0);
 }
--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -21,20 +21,43 @@
 
 # define SEC 1000000
 # define MSEC 1000
-# define NB_PHILO 3
-# define TIME_TO_DIE 10000
-# define TIME_TO_EAT 1000
-# define TIME_TO_SLEEP 1000
-# define MUST_EAT 10
 
-typedef struct s_philo
+# define RED     "\x1b[31m"
+# define GREEN   "\x1b[32m"
+# define BLUE    "\x1b[34m"
+# define RESET   "\x1b[0m"
+
+
+# define ISDIGIT(c) (c >= '0' && c <= '9')
+
+typedef struct		s_args
+{
+	unsigned int	number_of_philosopher;
+	unsigned int	time_to_die;
+	unsigned int	time_to_eat;
+	unsigned int	time_to_sleep;
+	unsigned int	must_eat;
+}					t_args;
+
+typedef struct 		s_philo
 {
 	pthread_t		thread_id;
 	int				thread_nb;
 	pthread_mutex_t	left_fork;
 	pthread_mutex_t	right_fork;
-	int				meals;
+	unsigned int	meals;
 	struct timeval	last_meal;
+	t_args			*args;
 }					t_philo;
+
+
+size_t				ft_strlen(const char *str);
+int					ft_atoi(const char *str);
+void				ft_putstr_fd(char const *s, int fd);
+char				*ft_itoa(int n);
+t_philo				**create_philos(t_args *args);
+int					free_philos(t_philo **philos, t_args *args);
+void				*thread_start(void	*arg);
+void				*death_thread(void *arg);
 
 #endif
